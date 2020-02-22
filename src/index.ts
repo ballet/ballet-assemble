f73fca21541fe8aea20d741da27d62d5600a7966
Micah Smith <micahjsmith@gmail.com>
2020-02-22 15:41:56 -0500

    Add initial button from tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,49 @@
 import {
+  IDisposable, DisposableDelegate
+} from '@phosphor/disposable';
+
+import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+
+import {
+  ToolbarButton
+} from '@jupyterlab/apputils';
+
+import {
+  DocumentRegistry
+} from '@jupyterlab/docregistry';
+
+import {
+  NotebookActions, NotebookPanel, INotebookModel
+} from '@jupyterlab/notebook';
+
+
+/**
+ * A notebook widget extension that adds a button to the toolbar.
+ */
+export
+class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  /**
+   * Create a new extension object.
+   */
+  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+    let callback = () => {
+      NotebookActions.runAll(panel.content, context.session);
+    };
+    let button = new ToolbarButton({
+      className: 'myButton',
+      iconClassName: 'fa fa-fast-forward',
+      onClick: callback,
+      tooltip: 'Run All'
+    });
+
+    panel.toolbar.insertItem(0, 'runAll', button);
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
+  }
+}
 
 
 /**
@@ -11,6 +54,7 @@ const extension: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension ballet-submit is activated!');
+    app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
   }
 };
 
