ac38c4b9ce97e9856af4d64ac2c35ff7b6ec3d8e
Micah Smith <micahjsmith@gmail.com>
2020-03-24 14:50:18 -0400

    update installation

--- Makefile
+++ Makefile
@@ -94,3 +94,11 @@ dist: clean ## builds source and wheel package
 install: clean-build clean-pyc ## install the package to the active Python's site-packages
 	pip install .
 
+.PHONY: install-test
+install-test: clean-build clean-pyc ## install the package and test dependencies
+	pip  install .[test]
+
+.PHONY: install-develop
+install-develop: clean-build clean-pyc ## install the package in editable mode and dependencies for development
+	pip install -e .[dev]
+
--- setup.py
+++ setup.py
@@ -5,59 +5,59 @@ import os
 
 from setupbase import (
     create_cmdclass, install_npm, ensure_targets,
-    combine_commands, ensure_python, get_version,
+    combine_commands, ensure_python
 )
-import setuptools
+from setuptools import setup, find_packages
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
-# The name of the project
-name="ballet-submit-labextension"
-
 # Ensure a valid python version
-ensure_python(">=3.6")
-
-# Get our version
-version = '0.4.0'
-
-lab_path = os.path.join(HERE, "server", name, "labextension")
+ensure_python('>=3.6')
 
 # Representative files that should exist after a successful build
 jstargets = [
-    os.path.join(HERE, "lib", "serverextension.js"),
+    os.path.join(HERE, 'lib', 'serverextension.js'),
 ]
 
 package_data_spec = {
-    name: [
-        "*"
+    'ballet_submit_labextension': [
+        '*'
     ]
 }
 
 data_files_spec = [
-    ("share/jupyter/lab/extensions", lab_path, "*.tgz"),
-    ("etc/jupyter/jupyter_notebook_config.d", "server/jupyter-config", "ballet-submit-labextension.json"),
+    (
+        'share/jupyter/lab/extensions',
+        os.path.join(HERE, 'server', 'ballet_submit_labextension', 'labextension'),
+        '*.tgz',
+    ),
+    (
+        'etc/jupyter/jupyter_notebook_config.d',
+        os.path.join(HERE, 'server', 'jupyter-config'),
+        'ballet-submit-labextension.json',
+    ),
 ]
 
-cmdclass = create_cmdclass("jsdeps", 
+cmdclass = create_cmdclass('jsdeps',
     package_data_spec=package_data_spec,
     data_files_spec=data_files_spec
 )
 
-cmdclass["jsdeps"] = combine_commands(
-    install_npm(HERE, build_cmd="build:all", npm=["jlpm"]),
+cmdclass['jsdeps'] = combine_commands(
+    install_npm(HERE, build_cmd='build:all', npm=['jlpm']),
     ensure_targets(jstargets),
 )
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 install_requires = [
-    "jupyterlab>=2.0",
-    "black==19.10b0",
-    "ballet",
-    "funcy",
-    "pygithub",
-    "stacklog",
+    'jupyterlab>=2.0',
+    'black==19.10b0',
+    'ballet',
+    'funcy',
+    'pygithub',
+    'stacklog',
 ]
 
 test_requirements = [
@@ -80,18 +80,18 @@ development_requirements = [
     'wheel>=0.30.0',
 ]
 
-setup_args = dict(
-    name=name,
-    version=version,
-    url="https://github.com/HDI-Project/ballet-submit-labextension",
-    author="Micah Smith",
-    description="Submit ballet modules from within JupyterLab",
+setup(
+    name='ballet-submit-labextension',
+    version='0.4.0',
+    url='https://github.com/HDI-Project/ballet-submit-labextension',
+    author='Micah Smith',
+    description='Submit ballet modules from within JupyterLab',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     cmdclass=cmdclass,
-    packages=setuptools.find_packages(where='server',
-                                      include=['ballet_submit_labextension',
-                                               'ballet_submit_labextension.*']),
+    packages=find_packages(where='server',
+                           include=['ballet_submit_labextension',
+                                    'ballet_submit_labextension.*']),
     install_requires=install_requires,
     extras_require={
         'test': test_requirements,
@@ -99,20 +99,16 @@ setup_args = dict(
     },
     zip_safe=False,
     include_package_data=True,
-    license="MIT",
-    platforms="Linux, Mac OS X, Windows",
-    keywords=["Jupyter", "JupyterLab"],
+    license='MIT',
+    platforms='Linux, Mac OS X, Windows',
+    keywords=['Jupyter', 'JupyterLab'],
     classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Framework :: Jupyter",
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Jupyter',
     ],
 )
-
-
-if __name__ == '__main__':
-    setuptools.setup(**setup_args)
