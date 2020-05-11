f242f13548e0a8300e89b9278920b37571cce90b
Micah Smith <micahjsmith@gmail.com>
2020-05-11 13:51:49 -0400

    Add base test case for handlers

--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -1,6 +1,9 @@
 import pytest
 from traitlets.config import Config
 
+from notebook.tests.launchnotebook import NotebookTestBase
+
+from ballet_submit_labextension import load_jupyter_server_extension
 from ballet_submit_labextension.submit import BalletApp
 
 
@@ -15,3 +18,20 @@ def config():
 def test_app_config(config):
     app = BalletApp.instance(config=config)
     assert app.debug in {True, False}
+
+
+class BaseTestCase(NotebookTestBase):
+
+    @classmethod
+    def setup_class(cls):
+        super().setup_class()
+        load_jupyter_server_extension(cls.notebook)
+
+
+class BalletHandlersTest(BaseTestCase):
+
+    def test_status(self):
+        pass
+        # response = self.request('GET', '/ballet/status')
+        # d = response.json()
+        # assert 'OK' == d['status']
