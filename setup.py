e76efcc9ce5a9790d48ab86dbd906f4a969cff3b
Micah Smith <micahjsmith@gmail.com>
2020-09-08 17:46:16 -0400

    Bump version: 0.7.1 → 0.7.2

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ development_requirements = [
 
 setup_args = dict(
     name='ballet-submit-labextension',
-    version='0.7.1',
+    version='0.7.2',
     url='https://github.com/HDI-Project/ballet-submit-labextension',
     author='Micah Smith',
     description='Submit ballet modules from within JupyterLab',
